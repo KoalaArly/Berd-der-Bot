304d8e957997bbbd37cfb82d7d1c5fc5c8f63e90
phrouni <phro@uni-bremen.de>
2025-10-29 23:31:01 +0100

    added commands: !wiki & !superglide

--- main.py
+++ main.py
@@ -18,4 +18,15 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f"{bot.user.name} is starting")
 
+# !wiki - link zur apex movement wiki
+@bot.command()
+async def wiki(ctx):
+    await ctx.send("Hier der Link zur Movement Wiki: \nhttps://apexmovement.tech/wiki")
+
+# !superglide - link zum trainer und mokey video
+@bot.command()
+async def superglide(ctx):
+    await ctx.send("Hier der Link zum Superglide Trainer & Guide: \nTrainer - https://apexmovement.tech/superglidetrainer/ \nGuide - https://www.youtube.com/watch?v=_cP1YO5Idts")
+
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+
